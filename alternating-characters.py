a635c56e83b1536f5937b80f1a8e349fc8e7a7b0
Y003 <48012774+Y003@users.noreply.github.com>
2024-06-07 17:22:17 +0530

    Add files via upload
    The solution to the problem - 'Alternating Characters'.

---
+++ alternating-characters.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    stack = [s[0]]
+    for i in range(1, len(s)):
+        if stack[-1] != s[i]:
+            stack.append(s[i])
+    
+    return len(s) - len(stack)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
