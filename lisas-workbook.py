64f28d01926fba466bfd05544efcfc5a52561a1c
Y003 <48012774+Y003@users.noreply.github.com>
2024-06-20 00:21:29 +0530

    Solution to problem - "Lisa's Workbook"
    A workbook contains exercise problems, grouped into chapters. Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located.

---
+++ lisas-workbook.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    # Write your code here
+    page = 0
+    count = 0
+    for i in range(n):
+        page += 1
+        for j in range(1, arr[i] + 1):
+            if j == page:
+                count += 1
+            if j != 0 and  j % k == 0 and j < arr[i]:
+                page += 1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
