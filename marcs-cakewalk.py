131c6e479c1d331f89e6300b42adced860acc833
Y003 <48012774+Y003@users.noreply.github.com>
2024-07-11 18:21:46 +0530

    Solution to the challenge - "Marc's Cakewalk"
    It was indeed a cakewalk!

---
+++ marcs-cakewalk.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    # Write your code here
+    calorie.sort(reverse = True)
+    minCalorieBurn = 0
+    for i in range(len(calorie)):
+        minCalorieBurn += (math.pow(2, i) * calorie[i])
+
+    return int(minCalorieBurn)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
